ae0f3fce95c2bb5cc2c126c7bcc168f92dc70283
Iqbal Fauzi <iqbalfawz@gmail.com>
2018-09-14 09:01:21 +0700

    Ignore filter on value is null

--- modules/lib-validator/filter/General.php
+++ modules/lib-validator/filter/General.php
@@ -10,38 +10,38 @@ namespace LibValidator\Filter;
 class General
 {
     static function array($value, $options, $object, $field, $filters){
-        return (array)$value;
+        return is_null($value) ? $value : (array)$value;
     }
 
     static function boolean($value, $options, $object, $field, $filters){
-        return (bool)$value;
+        return is_null($value) ? $value : (bool)$value;
     }
 
     static function float($value, $options, $object, $field, $filters){
-        return (float)$value;
+        return is_null($value) ? $value : (float)$value;
     }
 
     static function integer($value, $options, $object, $field, $filters){
-        return (int)$value;
+        return is_null($value) ? $value : (int)$value;
     }
 
     static function lowercase($value, $options, $object, $field, $filters){
-        return strtolower($value);
+        return is_null($value) ? $value : strtolower($value);
     }
 
     static function object($value, $options, $object, $field, $filters){
-        return (object)$value;
+        return is_null($value) ? $value : (object)$value;
     }
 
     static function string($value, $options, $object, $field, $filters){
-        return (string)$value;
+        return is_null($value) ? $value : (string)$value;
     }
 
     static function ucwords($value, $options, $object, $field, $filters){
-        return ucwords($value);
+        return is_null($value) ? $value : ucwords($value);
     }
 
     static function uppercase($value, $options, $object, $field, $filters){
-        return strtoupper($value);
+        return is_null($value) ? $value : strtoupper($value);
     }
 }
